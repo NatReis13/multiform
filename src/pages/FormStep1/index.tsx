f03fff6d1657d96fa146974701409c98c3e9f6fe
NatReis13 <natan-12rs@hotmail.com>
2022-08-22 16:24:35 -0300

    page 2

--- src/pages/FormStep1/index.tsx
+++ src/pages/FormStep1/index.tsx
@@ -1,30 +1,56 @@
 import { useNavigate } from "react-router-dom";
 import * as C from "./styles";
+import { useForm, FormActions } from "../../contexts/FormContext";
 import { Themer } from "../../components/Theme";
+import { ChangeEvent, useEffect } from "react";
 
 export const FormStep1 = () => {
-const navigate = useNavigate();
+  const navigate = useNavigate();
+  const { state, dispatch } = useForm(); //ler e execultar o nome na proxima page
 
-const handleNextStep = () =>{
-navigate('/step2');
+  useEffect(() => {
+    dispatch({
+      type: FormActions.setCurrentStep,
+      payload: 1,
+    }); // atualizar e mostrar o passo que esta
+  });
 
-}
+  const handleNextStep = () => {
+    if (state.name !== "") {
+      navigate("/step2"); //só passa de pagina se estiver diferente de vazio
+    }else{
+        alert("❌ Preencha os dados.");
+    };
+  };
+
+  const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    dispatch({
+      type: FormActions.setName,
+      payload: e.target.value,
+    }); // mandar o valor que esta no campo digitado
+  };
 
   return (
     <Themer>
       <C.Container>
-        <p>Passo 1/3</p>
+        <p>Passo 1/3 - {state.name}</p>
         <h1>Vamos começar com seu nome</h1>
         <p>Preencha o campo abaixo com seu nome completo.</p>
 
-        <hr/>
+        <hr />
 
         <label>
-            Seu nome completo:
-            <input type="text" autoFocus />
+          Seu nome completo:
+          <input
+            type="text"
+            autoFocus
+            value={state.name}
+            onChange={handleNameChange}
+          />
         </label>
 
         <button onClick={handleNextStep}>Próximo</button>
+        {state.currentStep}
       </C.Container>
     </Themer>
   );
--- src/pages/FormStep2/index.tsx
+++ src/pages/FormStep2/index.tsx
@@ -1,11 +1,63 @@
-import * as C from './styles';
-
+import { useNavigate } from "react-router-dom";
+import * as C from "./styles";
+import { useForm, FormActions } from "../../contexts/FormContext";
+import { Themer } from "../../components/Theme";
+import { ChangeEvent, useEffect } from "react";
+import { SelectOption } from "../../components/SelectOption";
 
 export const FormStep2 = () => {
-return (
-    <C.Container>
-        Passo 2
-    </C.Container>
-);
+  const navigate = useNavigate();
+  const { state, dispatch } = useForm(); //ler e execultar o nome na proxima page
 
+  useEffect(() => {
+    dispatch({
+      type: FormActions.setCurrentStep,
+      payload: 2,
+    }); // atualizar e mostrar o passo que esta
+  });
+
+  const handleNextStep = () => {
+    if (state.name !== "") {
+      navigate("/step3"); //só passa de pagina se estiver diferente de vazio
+    } else {
+      alert("❌ Preencha os dados.");
+    }
+  };
+
+  const setLevel= (level: number) => {
+    dispatch({
+        type: FormActions.setLevel,
+        payload: level
+    });// troca level selecionado e borda verde
+  }
+
+  return (
+    <Themer>
+      <C.Container>
+        <p>Passo 2/3 - {state.name}</p>
+        <h1>Vamos começar com seu nome</h1>
+        <p>Preencha o campo abaixo com seu nome completo.</p>
+
+        <hr />
+
+        <SelectOption
+          title="Sou iniciante"
+          description="Comecei a programar há menos de 2 anos"
+          icon="😅"
+          selected={state.level === 0}
+          onClick={setLevel(0)}
+        />
+
+        <SelectOption
+          title="Sou Programador"
+          description="Já programo há 2 anos ou mais"
+          icon="😎"
+          selected={state.level === 1}
+          onClick={setLevel(1)}
+        />
+
+        <button onClick={handleNextStep}>Próximo</button>
+      </C.Container>
+    </Themer>
+  );
 };
