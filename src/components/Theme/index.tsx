5c9c8ab002a3101b4cac1f9aa71f03053e9540cc
NatReis13 <natan-12rs@hotmail.com>
2022-08-23 15:36:11 -0300

    sidebaritem

--- src/components/Theme/index.tsx
+++ src/components/Theme/index.tsx
@@ -1,25 +1,50 @@
 import { ReactNode } from "react";
 import * as C from "./styles";
-import { Header}  from '../Header';
+import { Header } from "../Header";
+import { SidebarItem } from "../SidebarItem";
+import { useForm } from "../../contexts/FormContext";
+
 
 type Props = {
   children: ReactNode;
 };
 
-export const Themer = ({children}: Props) =>{
-    return(
-        <C.Container>
-<C.Area>
-    <Header/>
-    <C.Steps>
-        <C.Sidebar>
-✔
-        </C.Sidebar>
-        <C.Page>
-            {children}
-        </C.Page>
-    </C.Steps>
-</C.Area>
-        </C.Container>
-    )
-}
+export const Themer = ({ children }: Props) => {
+    const { state} = useForm();
+
+  return (
+    <C.Container>
+      <C.Area>
+        <Header />
+        <C.Steps>
+          <C.Sidebar>
+            <SidebarItem
+              title="Pessoal"
+              description="Se identifique"
+              icon="profile"
+              path="/"
+              active={state.currentStep === 1} //saber onde esta
+            />
+
+            <SidebarItem
+              title="Profissional"
+              description="Seu nivel"
+              icon="book"
+              path="/step2"
+              active={state.currentStep === 2} 
+            />
+
+            <SidebarItem
+              title="Contatos"
+              description="Como te achar"
+              icon="mail"
+              path="/step3"
+              active={state.currentStep === 3}
+            />
+          </C.Sidebar>
+          <C.Page>{children}</C.Page>
+        </C.Steps>
+      </C.Area>
+    </C.Container>
+  );
+};
